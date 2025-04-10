2cb8efd5f07bccf6755720b824c4d768a40bf8e7
Gordon Brander <gordonbrander@users.noreply.github.com>
2025-04-10 11:04:52 -0400

    Normalize headers, add cat method

--- src/headers.rs
+++ src/headers.rs
@@ -1,18 +1,29 @@
-use std::io::BufRead;
+use std::io::{BufRead, Cursor, Read, Write, copy};
 
 /// An HTTP-style header.
 pub struct Header {
-    pub key: String,
-    pub value: String,
+    key: String,
+    value: String,
 }
 
 impl Header {
     /// Create a new Header
+    /// Automatically converts the key and value to lowercase
     pub fn new(key: &str, value: &str) -> Self {
         Header {
-            key: key.to_string(),
-            value: value.to_string(),
+            key: key.trim().to_lowercase(),
+            value: value.trim().to_string(),
         }
+    }
+
+    /// Get the header key
+    pub fn key(&self) -> &str {
+        &self.key
+    }
+
+    /// Get the header value
+    pub fn value(&self) -> &str {
+        &self.value
     }
 
     /// Split the header value by commas
@@ -49,18 +60,8 @@ impl Headers {
                 break;
             }
 
-            // Find the colon separator
-            if let Some(index) = line.find(':') {
-                let (key, value) = line.split_at(index);
-
-                // Trim the key and skip the colon and any leading whitespace in the value
-                let key = key.trim();
-                let value = value[1..].trim();
-
-                headers.push(Header {
-                    key: key.to_string(),
-                    value: value.to_string(),
-                });
+            if let Some((key, value)) = line.split_once(":") {
+                headers.push(Header::new(key, value));
             }
         }
 
@@ -72,31 +73,61 @@ impl Headers {
     /// marks the end of headers.
     pub fn write_headers<W>(&self, writer: &mut W) -> std::io::Result<()>
     where
-        W: std::io::Write,
+        W: Write,
     {
         for header in &self.0 {
-            write!(writer, "{}: {}\r\n", header.key, header.value)?;
+            write!(writer, "{}: {}\r\n", header.key(), header.value())?;
         }
         // Write empty line to indicate end of headers
         write!(writer, "\r\n")?;
         Ok(())
     }
 
+    /// Concat reader contents to the end of headers.
+    /// Given a reader (typically source file) and a writer (typically a destination file)
+    /// serializes headers, writes them to writer, then copies reader contents to writer in
+    /// a streaming fashion.
+    pub fn cat<R, W>(&self, reader: &mut R, writer: &mut W) -> std::io::Result<()>
+    where
+        R: Read,
+        W: Write,
+    {
+        self.write_headers(writer)?;
+        copy(reader, writer)?;
+        Ok(())
+    }
+
     /// Get the first value of a header with the given key
+    /// Note: header keys are normalized to lowercase, so key should be lowercase.
     pub fn get_first_value(&self, key: &str) -> Option<&Header> {
-        self.0.iter().find(|header| header.key == key)
+        self.0.iter().find(|header| header.key() == key)
     }
 
     /// Get all values for a header with the given key
     /// In addition to collecting all headers with the given key, it also splits
     /// the header value by comma, per RFC 7230 section 3.2.2.
     /// <https://datatracker.ietf.org/doc/html/rfc7230>
+    /// Note: header keys are normalized to lowercase, so key should be lowercase.
     pub fn get_all_values(&self, key: &str) -> Vec<String> {
         self.0
             .iter()
-            .filter(|header| header.key == key)
+            .filter(|header| header.key() == key)
             .flat_map(|header| header.split_values())
             .collect()
+    }
+}
+
+impl From<&str> for Headers {
+    fn from(header_str: &str) -> Self {
+        let mut cursor = Cursor::new(header_str);
+        Headers::parse(&mut cursor)
+    }
+}
+
+impl From<String> for Headers {
+    fn from(header_str: String) -> Self {
+        let mut cursor = Cursor::new(header_str);
+        Headers::parse(&mut cursor)
     }
 }
 
@@ -120,10 +151,10 @@ mod tests {
         let headers = Headers::parse(&mut headers_reader);
 
         assert_eq!(headers.0.len(), 4);
-        assert_eq!(headers.0[0].key, "Host");
-        assert_eq!(headers.0[0].value, "example.com");
-        assert_eq!(headers.0[1].key, "User-Agent");
-        assert_eq!(headers.0[1].value, "Mozilla/5.0");
+        assert_eq!(headers.0[0].key(), "host");
+        assert_eq!(headers.0[0].value(), "example.com");
+        assert_eq!(headers.0[1].key(), "user-agent");
+        assert_eq!(headers.0[1].value(), "Mozilla/5.0");
 
         let mut body = String::new();
         _ = headers_reader.read_to_string(&mut body);
@@ -144,15 +175,15 @@ mod tests {
 
         let headers = Headers::parse(&mut headers_reader);
 
-        let first_host = headers.get_first_value("Host");
+        let first_host = headers.get_first_value("host");
         assert!(first_host.is_some());
         assert_eq!(first_host.unwrap().value, "example.com");
 
-        let first_user_agent = headers.get_first_value("User-Agent");
+        let first_user_agent = headers.get_first_value("user-agent");
         assert!(first_user_agent.is_some());
         assert_eq!(first_user_agent.unwrap().value, "Mozilla/5.0");
 
-        let non_existent = headers.get_first_value("Non-Existent");
+        let non_existent = headers.get_first_value("non-existent");
         assert!(non_existent.is_none());
     }
 
@@ -168,7 +199,7 @@ mod tests {
 
         let headers = Headers::parse(&mut headers_reader);
 
-        let all_accept_values = headers.get_all_values("Accept");
+        let all_accept_values = headers.get_all_values("accept");
         assert_eq!(
             all_accept_values,
             vec![
@@ -178,7 +209,7 @@ mod tests {
             ]
         );
 
-        let non_existent = headers.get_all_values("Non-Existent");
+        let non_existent = headers.get_all_values("non-existent");
         assert!(non_existent.is_empty());
 
         let mut body = String::new();
@@ -203,11 +234,35 @@ mod tests {
         let mut writer: Vec<u8> = Vec::new();
         headers.write_headers(&mut writer).unwrap();
 
-        let expected_headers = "Host: example.com\r\n\
+        let expected_headers = "host: example.com\r\n\
+            user-agent: Mozilla/5.0\r\n\
+            accept: text/html\r\n\
+            connection: keep-alive\r\n\
+            \r\n";
+
+        assert_eq!(String::from_utf8(writer).unwrap(), expected_headers);
+    }
+
+    #[test]
+    fn test_cat() {
+        let mut content_reader = Cursor::new("Foo".as_bytes());
+
+        let headers = Headers::from(
+            "Host: example.com\r\n\
             User-Agent: Mozilla/5.0\r\n\
             Accept: text/html\r\n\
-            Connection: keep-alive\r\n\
-            \r\n";
+            Connection: keep-alive\r\n",
+        );
+
+        let mut writer: Vec<u8> = Vec::new();
+        headers.cat(&mut content_reader, &mut writer).unwrap();
+
+        let expected_headers = "host: example.com\r\n\
+            user-agent: Mozilla/5.0\r\n\
+            accept: text/html\r\n\
+            connection: keep-alive\r\n\
+            \r\n\
+            Foo";
 
         assert_eq!(String::from_utf8(writer).unwrap(), expected_headers);
     }
