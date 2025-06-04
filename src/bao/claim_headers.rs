1d205720eee7cb63bc7d0287f31463d617540c7f
Gordon Brander <gordonbrander@users.noreply.github.com>
2025-06-04 16:30:11 -0400

    Make ClaimHeader Eq

--- src/bao/claim_headers.rs
+++ src/bao/claim_headers.rs
@@ -1,7 +1,13 @@
 use crate::claim::Claim;
 use serde::{Deserialize, Serialize};
 
-#[derive(Debug, Clone, Serialize, Deserialize)]
+#[derive(Debug, Clone, Serialize, Deserialize, PartialEq, Eq)]
 pub struct ClaimHeaders {
     pub claims: Vec<Claim>,
+}
+
+impl Default for ClaimHeaders {
+    fn default() -> Self {
+        Self { claims: Vec::new() }
+    }
 }
